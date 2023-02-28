01ad532207edabe507dd51bbbdff6beb73293283
yamrire <yamrire@student.1337.ma>
2023-03-01 00:01:08 +0100

    Rearrange files

---
+++ parsing/utils.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 02:25:12 by yamrire           #+#    #+#             */
+/*   Updated: 2023/02/28 23:56:50 by yamrire          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	handle_error(int static_code)
+{
+	perror("minishell");
+	exit(static_code);
+}
+
+void	free_double(char **pointer)
+{
+	int	i;
+
+	i = 0;
+	while (pointer[i])
+	{
+		free(pointer[i]);
+		i++;
+	}
+	free (pointer);
+	pointer = NULL;
+}
