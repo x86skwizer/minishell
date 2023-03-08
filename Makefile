7305d6230e6f4ad1f5ba5551ed0cb305738c5763
yamrire <yamrire@student.1337.ma>
2023-03-08 08:02:18 +0100

    Move check path funcs

--- Makefile
+++ Makefile
@@ -6,14 +6,15 @@
 #    By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/20 20:11:20 by yamrire           #+#    #+#              #
-#    Updated: 2023/03/07 03:59:52 by yamrire          ###   ########.fr        #
+#    Updated: 2023/03/08 08:00:54 by yamrire          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC = main.c ./parsing/parsing.c ./parsing//utils/ft_split.c ./parsing/utils/ft_strlen.c ./parsing/utils/ft_strnstr.c \
 	./parsing/utils/ft_substr.c ./parsing/utils/ft_strjoin.c ./parsing/utils/ft_strdup.c ./parsing/utils/ft_strchr.c ./parsing/utils.c \
 	./parsing/utils/ft_strcmp.c ./parsing/utils/ft_lstadd_back.c ./parsing/utils/ft_lstlast.c ./parsing/utils/ft_lstnew.c ./execution/utils/env_fill.c \
-	./execution/builtins/cd.c ./execution/builtins/echo.c ./execution/builtins/pwd.c ./execution/utils/libft_str.c ./execution/utils/search.c
+	./execution/builtins/cd.c ./execution/builtins/echo.c ./execution/builtins/pwd.c ./execution/utils/libft_str.c ./execution/utils/search.c \
+	./parsing/check_path.c
 NAME = minishell
 CC = cc
 FLAGS =  -lreadline -Wall -Werror -Wextra #-fsanitize=address -g3
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,87 +6,11 @@
 /*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 01:07:47 by yamrire           #+#    #+#             */
-/*   Updated: 2023/03/07 02:54:05 by yamrire          ###   ########.fr       */
+/*   Updated: 2023/03/08 07:59:17 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../merge.h"
-
-char	**get_paths(char **envp)
-{
-	char	*path_var;
-	char	**paths;
-	int		i;
-
-	i = 0;
-	while (envp[i])
-	{
-		path_var = ft_strnstr(envp[i], "PATH=", 5);
-		if (path_var)
-		{
-			path_var = ft_substr(path_var, 5, ft_strlen(path_var) - 5);
-			break ;
-		}
-		i++;
-	}
-	if (!path_var)
-		return (NULL);
-	paths = ft_split(path_var, ':');
-	free(path_var);
-	return (paths);
-}
-
-char	**arrange_paths(char **envp)
-{
-	char	**paths;
-	char	*tmp;
-	int		i;
-
-	paths = get_paths(envp);
-	if (!paths)
-		return (NULL);
-	i = 0;
-	while (paths[i])
-	{
-		tmp = paths[i];
-		paths[i] = ft_strjoin(paths[i], "/");
-		free(tmp);
-		i++;
-	}
-	return (paths);
-}
-
-void	add_cmd_path(t_pars **cmd)
-{
-	char	*path_cmd;
-	char	*tmp;
-	int		i;
-
-	if (!(*cmd)->paths)
-		return ;
-	tmp = ft_strdup((*cmd)->cmd[0]);
-	if (!(ft_strchr((*cmd)->cmd[0], '/')))
-	{
-		i = 0;
-		while ((*cmd)->paths[i])
-		{
-			path_cmd = ft_strjoin((*cmd)->paths[i], (*cmd)->cmd[0]);
-			if (access(path_cmd, F_OK | X_OK) == 0)
-			{
-				free((*cmd)->cmd[0]);
-				(*cmd)->cmd[0] = ft_strdup(path_cmd);
-				free(tmp);
-				free(path_cmd);
-				return ;
-			}
-			free(path_cmd);
-			i++;
-		}
-		free((*cmd)->cmd[0]);
-		(*cmd)->cmd[0] = ft_strdup(tmp);
-		free(tmp);
-	}
-}
 
 int	count_arg(char *str)
 {
