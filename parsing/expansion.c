a8d31745d306d715e800aea63927c5ea534ae93b
yamrire <yamrire@student.1337.ma>
2023-03-16 07:12:42 +0100

    expand 0

--- parsing/expansion.c
+++ parsing/expansion.c
@@ -6,7 +6,7 @@
 /*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 07:38:56 by yamrire           #+#    #+#             */
-/*   Updated: 2023/03/09 08:43:26 by yamrire          ###   ########.fr       */
+/*   Updated: 2023/03/16 07:11:16 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	check_expansion(char *cmd)
 	while (cmd[i])
 	{
 		if ((cmd[i] == '$') && (((cmd[i + 1] >= 65 && cmd[i + 1] <= 90)
-			|| (cmd[i + 1] >= 97 && cmd[i + 1] <= 122) || cmd[i + 1] == '_')))
+			|| (cmd[i + 1] >= 97 && cmd[i + 1] <= 122) || cmd[i + 1] == '_' || cmd[i + 1] == '?')))
 			return (1);
 		i++;
 	}
@@ -32,7 +32,8 @@ int	check_env_var(char *s)
 	t_list	*list;
 	t_env	*env;
 
-
+	if (s[0] == '?')
+		return (1);
 	list = my_global->env;
 	while (list)
 	{
@@ -48,8 +49,19 @@ void	expand(char **s)
 {
 	t_list	*list;
 	t_env	*env;
+	char	*s1;
+	char	*s2;
 
 
+	if ((*s)[0] == '?')
+	{
+		s1 = ft_itoa(my_global->exit_code);
+		s2 = ft_strdup((*s) + 1);
+		free(*s);
+		(*s) = ft_strjoin(s1, s2);
+		free(s1);
+		free(s2);
+	}
 	list = my_global->env;
 	while (list)
 	{
@@ -76,12 +88,12 @@ void	expand_arg(char **arg)
 	while ((*arg)[i])
 	{
 		if (((*arg)[i] == '$') && ((((*arg)[i + 1] >= 65 && (*arg)[i + 1] <= 90)
-			|| ((*arg)[i + 1] >= 97 && (*arg)[i + 1] <= 122)) || (*arg)[i + 1] == '_'))
+			|| ((*arg)[i + 1] >= 97 && (*arg)[i + 1] <= 122)) || (*arg)[i + 1] == '_' || (*arg)[i + 1] == '?'))
 		{
 			i++;
 			s1 = ft_substr((*arg), 0, i - 1);
 			start = i;
-			while ((((*arg)[i] >= 65 && (*arg)[i] <= 90) || ((*arg)[i] >= 97 && (*arg)[i] <= 122)) || (*arg)[i] == '_')
+			while ((((*arg)[i] >= 65 && (*arg)[i] <= 90) || ((*arg)[i] >= 97 && (*arg)[i] <= 122)) || (*arg)[i] == '_' || (*arg)[i] == '?')
 				i++;
 			s2 = ft_substr((*arg), start, i - start);
 			s3 = ft_substr((*arg), i, ft_strlen((*arg)) - i);
