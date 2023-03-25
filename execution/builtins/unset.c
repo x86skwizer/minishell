abaa7db757f1df84b701f5d6f1bc3559532d46b0
Yassine Amrire <yamrire@e2r3p10.1337.ma>
2023-03-25 05:53:40 +0000

    fix del node

--- execution/builtins/unset.c
+++ execution/builtins/unset.c
@@ -12,46 +12,35 @@ void	tenv_free(void *content)
 }
 
 
-int	builtin_unset( char **args)
+int    builtin_unset( char **args)
 {
-	t_list	*tmp;
-	t_env	*env_tmp;
-	int		i;
+    t_list    *tmp;
+    t_env    *env_tmp;
+    int        i;
 
-	i = 1;
-	tmp = g_global.env;
-	while (tmp)
-	{
-		env_tmp = tmp->content;
-		ft_putstr_fd(env_tmp->key, STDOUT_FILENO);
-		ft_putstr_fd("=", STDOUT_FILENO);
-		ft_putendl_fd(env_tmp->value, STDOUT_FILENO);
-		tmp = tmp->next;
-	}
-	while (args[i])
-	{
-		tmp = g_global.env;
-		while (tmp)
-		{
-			env_tmp = (t_env *)(tmp->content);
-			if (ft_strncmp(env_tmp->key, args[i], ft_strlen(env_tmp->key)) == 0)
-			{
-				ft_lstdelone(tmp, tenv_free);
-				break ;
-			}
-			tmp = tmp->next;
-		}
-		i++;
-	}
-	tmp = g_global.env;
-	printf("-----------after unset------------\n");
-	while (tmp)
-	{
-		env_tmp = tmp->content;
-		ft_putstr_fd(env_tmp->key, STDOUT_FILENO);
-		ft_putstr_fd("=", STDOUT_FILENO);
-		ft_putendl_fd(env_tmp->value, STDOUT_FILENO);
-		tmp = tmp->next;
-	}
-	return (0);
+    i = 1;
+    tmp = g_global.env;
+    while (args[i])
+    {
+        tmp = g_global.env;
+        t_list *parent = NULL;
+        while (tmp)
+        {
+            env_tmp = (t_env *)(tmp->content);
+            if (ft_strcmp(env_tmp->key, args[i]) == 0)
+            {
+                if (parent)
+                    parent->next = tmp->next;
+                else
+                    g_global.env = tmp->next;
+                ft_lstdelone(tmp, tenv_free);
+                break ;
+            }
+            parent = tmp;
+            tmp = tmp->next;
+        }
+        i++;
+    }
+    tmp = g_global.env;
+    return (0);
 }
