10878b0f59cf47beb2a93decef79b4ef067daaba
Yassine Amrire <yamrire@e2r3p10.1337.ma>
2023-03-25 03:39:23 +0000

    cd fixed

--- execution/builtins/cd.c
+++ execution/builtins/cd.c
@@ -1,27 +1,31 @@
 #include "../../merge.h"
 
-int	builtin_cd(char **args)
+int    builtin_cd(char **args)
 {
-	char	*old_path;
-	char	*new_path;
-	int		ret;
+    char    *old_path;
+    char    *new_path;
+    char *tmp;
+    int        ret;
 
-	old_path = getcwd(NULL, 0);
-	old_path = ft_strjoin(getcwd(old_path, 100), "/");
-	if (args[1] == NULL)
-		new_path = ft_strdup("/");
-	else
-		new_path = ft_strjoin(old_path, args[1]);
-	search_replace("OLDPWD", old_path);
-	search_replace("PWD", new_path);
-	ret = chdir(new_path);
-	if (ret < 0)
-	{
-		ft_putstr_fd("cd: ", STDERR_FILENO);
-		ft_putstr_fd(new_path, STDERR_FILENO);
-		ft_putendl_fd(" no such file or directory", STDERR_FILENO);
-		return (1);
-	}
-	old_path = getcwd(NULL, 0);
-	return (ret);
+    old_path = getcwd(NULL, 0);
+    tmp = ft_strjoin(getcwd(old_path, 100), "/");
+    free(old_path);
+    old_path = tmp;
+    if (args[1] == NULL)
+        new_path = ft_strdup("/");
+    else
+        new_path = ft_strjoin(old_path, args[1]);
+    search_replace("OLDPWD", old_path);
+    search_replace("PWD", new_path);
+    ret = chdir(new_path);
+    if (ret < 0)
+    {
+        ft_putstr_fd("cd: ", STDERR_FILENO);
+        ft_putstr_fd(new_path, STDERR_FILENO);
+        ft_putendl_fd(" no such file or directory", STDERR_FILENO);
+        return (1);
+    }
+    free(old_path);
+    free(new_path);
+    return (ret);
 }
