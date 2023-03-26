b36eb2b50f96d0211b6f89238a7c0f083028e8e9
yamrire <yamrire@student.1337.ma>
2023-03-26 18:36:56 +0000

    norm_unset

--- execution/builtins/unset.c
+++ execution/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:07:34 by yamrire           #+#    #+#             */
-/*   Updated: 2023/03/26 16:37:10 by yamrire          ###   ########.fr       */
+/*   Updated: 2023/03/26 18:35:59 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,40 @@ void	tenv_free(void *content)
 	free(env);
 }
 
+void	execute_unset(char **args, t_list *tmp, int i)
+{
+	t_list	*parent;
+	t_env	*env_tmp;
+
+	tmp = g_global.env;
+	parent = NULL;
+	while (tmp)
+	{
+		env_tmp = (t_env *)(tmp->content);
+		if (ft_strcmp(env_tmp->key, args[i]) == 0)
+		{
+			if (parent)
+				parent->next = tmp->next;
+			else
+				g_global.env = tmp->next;
+			ft_lstdelone(tmp, tenv_free);
+			break ;
+		}
+		parent = tmp;
+		tmp = tmp->next;
+	}
+}
+
 int	builtin_unset(char **args)
 {
 	t_list	*tmp;
-	t_env	*env_tmp;
-	t_list	*parent;
 	int		i;
 
 	i = 1;
 	tmp = g_global.env;
 	while (args[i])
 	{
-		tmp = g_global.env;
-		parent = NULL;
-		while (tmp)
-		{
-			env_tmp = (t_env *)(tmp->content);
-			if (ft_strcmp(env_tmp->key, args[i]) == 0)
-			{
-				if (parent)
-					parent->next = tmp->next;
-				else
-					g_global.env = tmp->next;
-				ft_lstdelone(tmp, tenv_free);
-				break ;
-			}
-			parent = tmp;
-			tmp = tmp->next;
-		}
+		execute_unset(args, tmp, i);
 		i++;
 	}
 	tmp = g_global.env;
