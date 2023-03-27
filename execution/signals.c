ab13f4c26fe16baeb6d34d404f7fd1b0e04f3d10
Yassine Amrire <yamrire@e2r3p8.1337.ma>
2023-03-27 01:26:25 +0000

    signals handled

--- execution/signals.c
+++ execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:08:37 by yamrire           #+#    #+#             */
-/*   Updated: 2023/03/26 16:08:39 by yamrire          ###   ########.fr       */
+/*   Updated: 2023/03/27 01:24:32 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	int_handler(int sig)
 void	quit_handler(int sig)
 {
 	(void)sig;
+	printf("\n");
 	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
